f2b83c8b052e19eb2407c85b281897b9cd662013
pczjh2 <joe.heeley@nottingham.ac.uk>
2025-02-10 13:36:28 +0000

    display on fail

--- src/js/app.js
+++ src/js/app.js
@@ -126,9 +126,8 @@ function searchCallback(err, options) {
 
     // Draw
     if (visualizationCheckbox.checked) {
-        console.log(options.iteration, parseFloat(iterationLimitInput.value))
         if (options.iteration > parseFloat(iterationLimitInput.value)) {
-            visualization.innerHTML = "<div style='display: flex; justify-content: center; align-items: center; height: 100vh; font-size: 2rem;'>The AI has failed to solve this puzzle within the iteration limit!</div>"
+            visualization.innerHTML = "<div style='display: flex; justify-content: center; align-items: center; height: 100vh; font-size: 2rem;'>Solution Not Found!</div>"
         }
         else {
             if (expandedNodesLength < 1500) {
